814bde490325d08c09f5488d07b6777d99237ca0
Tawheed-tariq <tavaheed_2022bite008@nitsri.ac.in>
2024-09-07 16:37:37 +0530

    array

---
+++ Structures/Array Questions/two-sum.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        for(int i = 0; i < nums.size(); i++){
+            int complement = target - nums[i];
+            if(mp.count(complement)){
+                return {mp[complement], i};
+            }
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
+};
