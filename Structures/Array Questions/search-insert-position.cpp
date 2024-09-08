885d4e7670edba22524d41e0cb705294d6e4e46f
Tawheed-tariq <tavaheed_2022bite008@nitsri.ac.in>
2024-09-08 06:38:30 +0530

    array

---
+++ Structures/Array Questions/search-insert-position.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int s = 0, e = nums.size() - 1;
+        int mid = s + (e-s)/2;
+        while(s <= e){
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] < target)
+                s = mid+1;
+            else 
+                e = mid -1;
+            mid = s + (e-s)/2;
+        }
+        return s;
+    }
+};
