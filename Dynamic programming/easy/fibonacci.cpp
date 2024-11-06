96ba546950b9493f5a733dad9ba0267c0a53456c
Tawheed-tariq <tavaheed_2022bite008@nitsri.ac.in>
2024-11-06 07:54:45 +0530

    DP

---
+++ Dynamic programming/easy/fibonacci.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int dp[40]{};
+    int fib(int n) {
+        if(n <= 1) return n;
+
+        if(dp[n]) return dp[n];
+
+        dp[n] = fib(n-1) + fib(n-2);
+        return dp[n];
+    }
+};
