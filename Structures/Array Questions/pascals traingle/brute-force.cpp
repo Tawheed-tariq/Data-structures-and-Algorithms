db3d0ac2da13423e414edd583b19ffde52d826e6
Tawheed-tariq <tavaheed_2022bite008@nitsri.ac.in>
2025-02-08 11:09:47 +0530

    pascal traingle

---
+++ Structures/Array Questions/pascals traingle/brute-force.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    public:
+        vector<vector<int>> generate(int numRows) {
+            vector<vector<int>> res;
+    
+            res.push_back({1});
+            for(int i = 1; i < numRows; i++){
+                vector<int> temp;
+                temp.push_back(1);
+                for(int j = 1; j < i; j++){
+                    vector<int> a = res[i-1];
+                    temp.push_back(a[j-1] + a[j]);
+                }
+                temp.push_back(1);
+                res.push_back(temp);
+            }
+            return res;
+        }
+    };
