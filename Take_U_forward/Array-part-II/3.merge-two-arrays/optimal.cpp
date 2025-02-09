7b8d5b21f654396e76a6e2398d5c73fc6dcfbc92
Tawheed-tariq <tavaheed_2022bite008@nitsri.ac.in>
2025-02-09 11:29:06 +0530

    merge two arrays

---
+++ Take_U_forward/Array-part-II/3.merge-two-arrays/optimal.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    public:
+        void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+            int ind1 = 0, ind2 = 0;
+            vector<int> ans;
+            while(ind1 < m && ind2 < n){
+                if(nums1[ind1] < nums2[ind2]){
+                    ans.push_back(nums1[ind1++]);
+                }
+                else{
+                    ans.push_back(nums2[ind2++]);
+                }
+            }
+            while(ind1 < m){
+                ans.push_back(nums1[ind1++]);
+            }
+            while(ind2 < n){
+                ans.push_back(nums2[ind2++]);
+            }
+            for(int i =0; i < m+n; i++){
+                nums1[i] = ans[i];
+            }
+        }
+    };
