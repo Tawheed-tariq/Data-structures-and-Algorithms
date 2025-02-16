54211cfaf344f8b14fb68d3d2794265cc0ba1ea1
Tawheed-tariq <tavaheed_2022bite008@nitsri.ac.in>
2025-02-16 07:46:40 +0530

    ones

---
+++ Take_U_forward/Linked-List and Arrays/6.consecutive onces/brute-force.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    public:
+        int findMaxConsecutiveOnes(vector<int>& nums) {
+            int ones = 0;
+            int ans = 0;
+            for(int i = 0; i < nums.size(); i++){
+                if(nums[i] == 1)
+                    ones++;
+                else{
+                    ans = max(ones, ans);
+                    ones = 0;
+                }
+            }
+            ans = max(ones, ans);
+            return ans;
+        }
+    };
