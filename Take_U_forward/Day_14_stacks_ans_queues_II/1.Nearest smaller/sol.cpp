ee504cc97ce44a5b8851b6e1440b404f97dda54d
Tawheed-tariq <tavaheed_2022bite008@nitsri.ac.in>
2025-02-26 19:44:34 +0530

    next smaller

---
+++ Take_U_forward/Day_14_stacks_ans_queues_II/1.Nearest smaller/sol.cpp
@@ -0,0 +1,13 @@
+vector<int> Solution::prevSmaller(vector<int> &A) {
+    stack<int> st;
+    vector<int> ans(A.size(), -1);
+    for(int i = 0; i < A.size(); i++){
+        while(st.size() > 0 && st.top() >= A[i]){
+            st.pop();
+        }
+        if(!st.empty())
+            ans[i] = st.top();
+        st.push(A[i]);
+    }
+    return ans;
+}
