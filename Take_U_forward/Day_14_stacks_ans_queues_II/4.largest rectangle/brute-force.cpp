038903ab07d3882d78d63a7af197349defe28ecf
Tawheed-tariq <tavaheed_2022bite008@nitsri.ac.in>
2025-02-28 22:30:31 +0530

    largest rectangle

---
+++ Take_U_forward/Day_14_stacks_ans_queues_II/4.largest rectangle/brute-force.cpp
@@ -0,0 +1,14 @@
+class Solution {
+    public:
+        int largestRectangleArea(vector<int>& heights) {
+            int maxArea = 0, n = heights.size();
+            for(int i = 0; i < n; i++){
+                int minHeight = INT_MAX;
+                for(int j = i; j< n; j++){
+                    minHeight = min(minHeight, heights[j]);
+                    maxArea = max(maxArea, minHeight*(j-i+1));
+                }
+            }
+            return maxArea;
+        }
+    };
