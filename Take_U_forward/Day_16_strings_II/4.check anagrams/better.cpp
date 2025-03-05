beb4adce2575d93d0bcc1f83a94a31ecbc79e2f6
Tawheed-tariq <tavaheed_2022bite008@nitsri.ac.in>
2025-03-05 07:08:50 +0530

    check anagrams

---
+++ Take_U_forward/Day_16_strings_II/4.check anagrams/better.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    public:
+        bool isAnagram(string s, string t) {
+            unordered_map<char, int> mp;
+    
+            for(auto x:s){
+                mp[x]++;
+            }
+            for(auto x:t){
+                mp[x]--;
+            }
+    
+            for(auto x: mp){
+                if(x.second != 0)
+                    return false;
+            }
+            return true;
+        }
+    };
