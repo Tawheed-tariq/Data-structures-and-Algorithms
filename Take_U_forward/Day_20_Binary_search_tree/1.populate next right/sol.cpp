4cfdf899a9470f05d56622b55e79eddf925aacd4
Tawheed-tariq <tavaheed_2022bite008@nitsri.ac.in>
2025-03-13 10:33:14 +0530

    populate next right

---
+++ Take_U_forward/Day_20_Binary_search_tree/1.populate next right/sol.cpp
@@ -0,0 +1,41 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+    public:
+        Node* connect(Node* root) {
+            if(!root) return root;
+            queue<Node *> q;
+            q.push(root);
+            q.push(NULL);
+    
+            while(!q.empty()){
+                Node *front = q.front();
+                q.pop();
+    
+                if(front != NULL){
+                    front->next = q.front();
+                    if(front->left) q.push(front->left);
+                    if(front->right) q.push(front->right);
+                }else{
+                    if(!q.empty()) q.push(NULL);
+                }
+            }
+            return root;
+        }
+    };
