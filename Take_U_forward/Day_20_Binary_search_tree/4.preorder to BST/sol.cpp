1bcf8d1ba3443e66a577ef50e37f309e48821b4e
Tawheed-tariq <tavaheed_2022bite008@nitsri.ac.in>
2025-03-13 11:00:25 +0530

    preorder to BST

---
+++ Take_U_forward/Day_20_Binary_search_tree/4.preorder to BST/sol.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+ class Solution {
+    public:
+        TreeNode *solve(vector<int> &preorder, int start, int end){
+            if(start > end) return NULL;
+            TreeNode *root = new TreeNode(preorder[start]);
+            int i = start+1;
+            while(i <= end && preorder[start] > preorder[i]) i++;
+    
+            root->left = solve(preorder, start+1, i-1);
+            root->right = solve(preorder, i, end);
+            return root;
+        }
+        TreeNode* bstFromPreorder(vector<int>& preorder) {
+            return solve(preorder, 0, preorder.size()-1);
+        }
+    };
