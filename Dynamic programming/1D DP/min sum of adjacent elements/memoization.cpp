3949e68024cdb86b5547feda8c4d2ae461e97258
Tawheed-tariq <tavaheed_2022bite008@nitsri.ac.in>
2025-04-23 06:32:28 +0530

    largest

--- Dynamic programming/1D DP/min sum of adjacent elements/memoization.cpp
+++ Dynamic programming/1D DP/min sum of adjacent elements/memoization.cpp
@@ -1,22 +1,16 @@
-#include<iostream>
-#include<bits/stdc++.h>
-using namespace std;
-int solve(vector<int> &nums, int index, vector<int> &dp){
-    if(index >= nums.size())
+#include <bits/stdc++.h> 
+int solve(vector<int> &nums, int ind, vector<int> &dp){
+    if(ind >= nums.size())
         return 0;
-    if(index == nums.size()-1)
-        return nums[nums.size() -1];
-    //include
-    if(dp[index] != -1)
-        return dp[index];
-    int inc = solve(nums, index+2, dp) + nums[index];
-    //exclude
-    int exc = solve(nums, index+1, dp) + 0;
-    dp[index] = max(inc, exc);
-    return dp[index];
+    
+    if(dp[ind] != -1)
+        return dp[ind];
+    int include = solve(nums, ind+2, dp) + nums[ind];
+    int excude = solve(nums, ind+1, dp);
+
+    return dp[ind] = max(include, excude);
 }
 int maximumNonAdjacentSum(vector<int> &nums){
-    vector<int> dp(nums.size(), -1);
-    int total = solve(nums, 0, dp);
-    return total;
+    vector<int> dp(nums.size() + 1, -1);
+    return solve(nums, 0, dp);
 }
