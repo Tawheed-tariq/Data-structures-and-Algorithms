72b2c453fa4ee5bd6963a75cd9f1db164e24b79c
Tawheed-tariq <tavaheed_2022bite008@nitsri.ac.in>
2025-05-14 10:48:45 +0530

    lis

---
+++ Dynamic programming/1D DP/Number of increasing subsequences/sol.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> length(n, 1);
+        vector<int> count(n, 1);
+
+        for(int i =1; i < n; i++){
+            for(int j =0; j < i; j++){
+                if(nums[j] < nums[i]){
+                    if(length[j]+1 > length[i]){
+                        length[i ] = length[j] + 1;
+                        count[i] = count[j];
+                    }else if(length[j] + 1 == length[i]){
+                        count[i] += count[j];
+                    }
+                } 
+            }
+        }
+
+        int max_len = INT_MIN;
+        for(auto ele: length){
+            max_len = max(ele, max_len);
+        }
+        int res= 0;
+        for(int i =0; i< count.size(); i++){
+            if(length[i] == max_len)
+                res += count[i];
+        }
+        return res;
+    }
+};
