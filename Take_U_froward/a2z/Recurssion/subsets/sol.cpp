ef44f91f2208badc2576f7f45cd1e3c8677d9d26
Tawheed-tariq <tavaheed_2022bite008@nitsri.ac.in>
2025-05-23 08:49:50 +0530

    subsets

---
+++ Take_U_froward/a2z/Recurssion/subsets/sol.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void solve(vector<int>& nums, int ind,vector<int> &temp, vector<vector<int>> &ans){
+        if(ind >= nums.size()){
+            ans.push_back(temp);
+            return;
+        }
+        
+        temp.push_back(nums[ind]);
+        solve(nums, ind+1, temp, ans);
+        temp.pop_back();
+
+        solve(nums, ind+1, temp, ans);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+
+        solve(nums, 0, temp, ans);
+        return ans;
+    }
+};
