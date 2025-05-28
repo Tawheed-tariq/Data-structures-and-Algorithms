0c3ab801e85030070e456f56134019ab85ec87f7
Tawheed-tariq <tavaheed_2022bite008@nitsri.ac.in>
2025-05-28 10:57:33 +0530

    window

---
+++ Take_U_froward/a2z/Sliding Window/subarrays with k diff elements/sol.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int solve(vector<int>& nums, int k){
+        int left= 0, count  = 0;
+        unordered_map<int, int> mp;
+
+        for(int right = 0; right < nums.size(); right++){
+            mp[nums[right]]++;
+
+            while(mp.size() > k){
+                mp[nums[left]]--;
+                if(mp[nums[left]] == 0)
+                    mp.erase(nums[left]);
+                left++;
+            }
+
+            count += right - left + 1;
+        }
+        return count;
+    }
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return solve(nums, k) - solve(nums, k-1);
+    }
+};
