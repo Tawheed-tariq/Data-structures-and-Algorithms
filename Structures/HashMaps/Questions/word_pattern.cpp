840eb142bb9eb72eb29e5a17e9c9e53e16d82872
Tawheed-tariq <tavaheed_2022bite008@nitsri.ac.in>
2025-06-12 19:14:12 +0530

    brute

---
+++ Structures/HashMaps/Questions/word_pattern.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        vector<string> vec;
+        string temp;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == ' '){
+                vec.push_back(temp);
+                temp = "";
+                continue;
+            }else if(i == s.size() -1){
+                temp.push_back(s[i]);
+                vec.push_back(temp);
+            }
+            temp.push_back(s[i]);
+        }
+
+        unordered_map<char, string> mp;
+        unordered_map<string, char> mapp;
+
+        if(pattern.size() != vec.size()) return false;
+
+        for(int i = 0; i < vec.size(); i++){
+            if(mp.find(pattern[i]) != mp.end()){
+                if(mp[pattern[i]] != vec[i]) return false;
+            }else{
+                mp[pattern[i]] = vec[i];
+            }
+
+            if(mapp.find(vec[i]) != mapp.end()){
+                if(mapp[vec[i]] != pattern[i]) return false;
+            }else{
+                mapp[vec[i]] = pattern[i];
+            }
+        }
+
+
+
+        return true;
+    }
+};
