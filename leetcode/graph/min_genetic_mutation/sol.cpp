6453274fb9827c9626e90bf9a17ec3bc12ceed78
Tawheed-tariq <tavaheed_2022bite008@nitsri.ac.in>
2025-08-11 07:05:13 +0530

    improvement

---
+++ leetcode/graph/min_genetic_mutation/sol.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int minMutation(string start, string end, vector<string>& bank) {
+        unordered_set<string> st{bank.begin(), bank.end()};
+        if(!st.count(end)) return -1;
+
+        queue<string> q;
+        q.push(start);
+        int steps = 0, s;
+        string curr , t;
+
+        while(!q.empty()){
+            s = q.size();
+            while(s--){
+                curr = q.front();
+                q.pop();
+
+                if(curr == end) return steps;
+
+                st.erase(curr);
+
+                for(int i = 0; i < 8; i++){
+                    t = curr;
+                    t[i] = 'A';
+                    if(st.count(t)) q.push(t);
+
+                    t[i] = 'C';
+                    if(st.count(t)) q.push(t);
+
+                    t[i] = 'G';
+                    if(st.count(t)) q.push(t);
+
+                    t[i] = 'T';
+                    if(st.count(t)) q.push(t);
+                }
+            }
+
+            steps++;
+        }
+        return -1;
+    }
+};
